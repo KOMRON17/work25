584b334406c9c920f4eb95e5ea4ce7fbb50d7e01
KOMRON <privetolp059@gmail.com>
2022-04-20 09:26:50 +0300

    Задание 1

--- index.php
+++ index.php
@@ -1,37 +1,18 @@
 <?php
 class Worker{
-	private $name;
-	private $age;
-	private $salary;
-	public function setName($n){
-	    $this->name= $n;
-	}
-    public function getName(){
-	    return $this->name;
-    }
-    public function setAge($a){
-	    $this->age= $a;
-    }
-    public function getAge(){
-	    return $this->age;
-    }
-    public function setSalary($s){
-	    $this->salary= $s;
-    }
-    public function getSalary(){
-	    return $this->salary;
-    }
+	public $name;
+	public $age;
+	public $salary;
 }
 $obj =new Worker();
-$obj->setName("Ivan");
-$obj->setAge(21);
-$obj->setSalary(1000);
+$obj->name="Ivan";
+$obj->age=21;
+$obj->salary=1000;
  
 $obj2 =new Worker();
-$obj2->setName("Ivan");
-$obj2->setAge(21);
-$obj2->setSalary(1000);
-
-echo "Summa age", $obj->getAge() + $obj2->getAge();
+$obj2->name="Vanya";
+$obj2->age=20;
+$obj2->salary=2000;
+echo "Summa age", $obj->age + $obj2->age;
 echo "<br>";
-echo "summa salary", $obj->getSalary()+$obj2->getSalary();
+echo "summa salary", $obj->salary+$obj2->salary;
